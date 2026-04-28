de88ed26574485db98a9f259c808e3318834340a
Giselle Castillo <gabicaztillo2670@gmail.com>
2026-04-28 18:54:41 -0500

    feat: agregar script bash de setup

---
+++ setup_project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# setup_project.sh
+
+echo "Actualizando paquetes..."
+echo "Instalando Git..."
+echo "Instalando Python3..."
+echo "Instalando Docker..."
+
+# Crear carpetas del proyecto
+mkdir -p app
+mkdir -p helper
+
+echo "Entorno preparado con éxito."
